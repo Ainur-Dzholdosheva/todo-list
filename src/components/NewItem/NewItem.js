21da6909f812382cca37f4178bc8d1e4ad2ea961
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-30 00:09:59 +0600

    add function

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -2,11 +2,19 @@ import React from "react";
 import classes from "./NewItem.module.css";
 import Button from "../UI/Button/Button";
 
-export default () => {
+export default ({ addItem }) => {
+  function addButtonClicked({ target }) {
+    const input = target.previousSibling;
+    addItem(input.value);
+    input.value = "";
+    input.focus();
+  }
   return (
     <div className={classes.NewItem}>
       <input type="text" />
-      <Button green>Add</Button>
+      <Button click={addButtonClicked} green>
+        Add
+      </Button>
     </div>
   );
 };
